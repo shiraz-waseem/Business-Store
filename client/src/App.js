452db99424683a239c629cbce49d326650d18b9c
Shiraz Waseem <shirazwaseem321@gmail.com>
2024-09-22 14:23:47 +0500

    Frontend completed

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      {/* <Home />
+      <ProductList /> */}
+      {/* <Product /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <Cart />
+    </>
+  );
+}
+
+export default App;
